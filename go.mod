dcf1f21325ae599eae4cb57a9f7cae1a02b340e3
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-09-19 01:59:04 +0900

    first import

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/mattn/oci-cluster-token
+
+go 1.24.1
+
+require github.com/oracle/oci-go-sdk v24.3.0+incompatible
+
+require github.com/stretchr/testify v1.8.4 // indirect
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/oracle/oci-go-sdk/common"
+)
+
+var outputTemplate = `{
+    "apiVersion": "client.authentication.k8s.io/v1beta1",
+    "kind": "ExecCredential",
+    "status": {
+        "token": "%s",
+        "expirationTimestamp": "%s"
+    }
+}`
+
+func main() {
+	var clusterID string
+	var region string
+	flag.StringVar(&clusterID, "cluster-id", "", "OCI Container Engine for Kubernetes Cluster OCID")
+	flag.StringVar(&region, "region", "", "OCI Region (e.g., us-ashburn-1)")
+	flag.Parse()
+
+	provider := common.DefaultConfigProvider()
+
+	if clusterID == "" {
+		log.Fatal("cluster-id is required")
+	}
+
+	if region == "" {
+		if tmp, err := provider.Region(); err == nil {
+			region = tmp
+		}
+	}
+	_url := fmt.Sprintf("https://containerengine.%s.oraclecloud.com/cluster_request/%s", region, clusterID)
+	req, err := http.NewRequest("GET", _url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	date := time.Now().UTC().Format(http.TimeFormat)
+	req.Header.Set("Date", date)
+
+	signer := common.DefaultRequestSigner(provider)
+	signer.Sign(req)
+
+	params := url.Values{}
+	params.Add("date", req.Header.Get("Date"))
+	params.Add("authorization", req.Header.Get("Authorization"))
+
+	fmt.Printf(outputTemplate,
+		base64.StdEncoding.EncodeToString([]byte(req.URL.String()+"?"+params.Encode())),
+		time.Now().UTC().Add(4*time.Minute).Format("2006-01-02T15:04:05Z"),
+	)
+}
